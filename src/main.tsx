085b1981aa1794bfff5072c4ca0c2c37f5283f19
Ess <127221085+EstherLavender@users.noreply.github.com>
2025-12-09 08:49:00 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { ThirdwebProvider } from "thirdweb/react";
+import { createThirdwebClient } from "thirdweb";
+import { avalancheFuji } from "thirdweb/chains";
+
+const client = createThirdwebClient({
+  clientId: import.meta.env.VITE_THIRDWEB_CLIENT_ID!, // set this in .env
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThirdwebProvider client={client} activeChain={avalancheFuji}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>,
+);
